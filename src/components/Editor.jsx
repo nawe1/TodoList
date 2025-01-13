fd2474067dfb855deebcc7bcd7507a8f00f01a5e
Bumsoo <161221357+nawe1@users.noreply.github.com>
2025-01-13 11:58:45 +0900

    수정중....

---
+++ src/components/Editor.jsx
@@ -0,0 +1,40 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+  return (
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo"
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
